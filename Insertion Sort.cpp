22200f33e9df66236e05397e3eceb0353233d03c
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-28 18:58:53 -0300

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -1,16 +1,21 @@
 void insertionSort(int vetor[], int tamanhoVetor) {
 
-int escolhido, j;
+int escolhido, j, cont=0;
+
+auto start = chrono::steady_clock::now(); //comando para medir o tempo de execução do codigo
 
     for (int i = tamanhoVetor; i >= 0; i--) {
+        cont++
 		escolhido = vetor[i];
 		j = i + 1;
 
 		while ((j <= tamanhoVetor-1) && (vetor[j] < escolhido)) {
+            cont++
 			vetor[j - 1] = vetor[j];
 			j++;
 		}
 
 		vetor[j - 1] = escolhido;
 	}
+	cout<< "tempo de execução em segundos: "<<chrono::duration_cast<chrono::seconds>(end - start).count()<<"S"<<endl;//retorno do comando para medir o tempo de execução do codigo em segundos
 }
