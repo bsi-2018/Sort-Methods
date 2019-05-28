55c799b24e3ca27a27a094f36a88d14f6c24123b
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-27 21:50:19 -0300

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -25,38 +25,3 @@ int escolhido, j;
 		vetor[j - 1] = escolhido;
 	}
 }
-int main()
-{
-    setlocale(LC_ALL,"");
-    srand(time(NULL));
-    int tamanhoVetor=100;
-
-    cout << "Programa de Ordenação Insertion Sort (penultima casa)" << endl
-         << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" << endl
-         << "Gerando vetor de "<<tamanhoVetor<<" números aleatórios: "<<endl;
-
-    int *vetor = new int[tamanhoVetor];
-
-    for (int i = 0; i < tamanhoVetor; i++){
-        vetor[i] = rand()%1001;
-    }
-
-    cout << "Pronto!" << endl;
-
-    cout << endl << "Números gerados: [";
-    for (int i = 0; i < tamanhoVetor; i++)
-        cout << vetor[i] << " ";
-    cout << "]" << endl;
-
-    cout << "Ordenando... " << endl;
-
-    insertionSort(vetor, tamanhoVetor);
-
-    cout << "Vetor Ordenado" << endl << "[";
-    for (int i = 0; i < tamanhoVetor; i++)
-        cout << vetor[i] << " ";
-    cout << "]" << endl;
-
-    delete vetor;
-    return 0;
-}
