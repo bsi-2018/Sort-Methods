11eb793e39b14161d924662a4cd6ec6f69a2184d
ThyagoGazolla <49158691+ThyagoGazolla@users.noreply.github.com>
2019-05-27 21:53:25 -0300

    Update Shell Sort.cpp

--- Shell Sort.cpp
+++ Shell Sort.cpp
@@ -5,46 +5,63 @@
 #include<string.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<time.h>
 
 using namespace std;
 
-int main()
-{
+void shellSort(int vetor[], int tam){
 
-    setlocale(LC_ALL, "portuguese");
+    int aux, j;
 
-    int vetor[1000], aux, j, tam;
-
-    cout<<"Digite o tamanho do vetor: "<<endl;
-    cin>>tam;
-
-    for(int i = 0; i < tam; i++)
-    {
-        cin>>vetor[i];
-    }
-
-    for (int gap = tam/2; gap > 0; gap /= 2)
-    {
+    for (int gap = tam/2; gap > 0; gap /= 2){
         for (int i = gap; i < tam; i += 1)
         {
             aux = vetor[i];
 
-            for (j = i; j >= gap && vetor[j - gap] > aux; j = j - gap)
-            {
+            for (j = i; j >= gap && vetor[j - gap] > aux; j = j - gap){
                 vetor[j] = vetor[j - gap];
             }
-
             vetor[j] = aux;
         }
     }
+}
 
-    cout<<endl;
+int main()
+{
 
-    for (int i = 0; i < tam; i++)
-    {
-        cout << vetor[i]<<endl;
+    setlocale(LC_ALL,"");
+    srand(time(NULL));
+    int qtde=100;
+
+    cout << "Programa de Ordenação Insertion Sort (penultima casa)" << endl
+         << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" << endl
+         << "Gerando vetor de "<<qtde<<" números aleatórios: "<<endl;
+
+    int *vetor = new int[qtde];
+
+    for (int i = 0; i < qtde; i++){
+        vetor[i] = rand()%1001;
     }
 
-system("pause");
-return 0;
+    cout << "Pronto!" << endl;
+
+    cout << endl << "Números gerados: [";
+    for (int i = 0; i < qtde; i++)
+        cout << vetor[i] << " ";
+    cout << "]" << endl;
+
+    cout << "Ordenando... " << endl;
+
+    int passos = 0;
+    shellSort(vetor, qtde);
+
+    cout <<"Número de passos: "<< passos << " passos." << endl;
+
+    cout << "Vetor Ordenado" << endl << "[";
+    for (int i = 0; i < qtde; i++)
+        cout << vetor[i] << " ";
+    cout << "]" << endl;
+
+    delete vetor;
+    return 0;
 }
