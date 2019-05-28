2662706f82cd1ba926d2d1fc0ea0532e5f58ee19
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-28 18:58:49 -0300

    Update Heap Sort.cpp

--- Heap Sort.cpp
+++ Heap Sort.cpp
@@ -2,40 +2,50 @@
 #include<windows.h>
 #include<math.h>
 #include<locale.h>
+#include<chrono>
 #include<string.h>
 #include<stdlib.h>
 #include<stdio.h>
+#include<chrono>
 
 using namespace std;
 
 void heap(int vetor[], int tam, int i);
 void heapSort(int vetor[], int tam);
 
-
-
 void heap(int vetor[], int tam, int i)
 {
+
     int largest = i;
     int l = 2*i + 1;
     int r = 2*i + 2;
+    int cont=0;
+
+    auto start = chrono::steady_clock::now(); //comando para medir o tempo de execução do codigo
 
     if (l < tam && vetor[l] > vetor[largest])
+        cont++;
         largest = l;
 
     if (r < tam && vetor[r] > vetor[largest])
+        cont++;
         largest = r;
 
-       if (largest != i)
+    if (largest != i)
     {
         swap(vetor[i], vetor[largest]);
-
+        cont++;
         heap(vetor, tam, largest);
     }
+     cout<< "tempo de execução em segundos: "<<chrono::duration_cast<chrono::seconds>(end - start).count()<<"S"<<endl;//retorno do comando para medir o tempo de execução do codigo em segundos
 }
 
 void heapSort(int vetor[], int tam)
 {
+    int cont2=0;
+    auto start = chrono::steady_clock::now(); //comando para medir o tempo de execução do codigo
     for (int i = tam / 2 - 1; i >= 0; i--)
+        cont2++;
         heap(vetor, tam, i);
 
 
@@ -43,7 +53,9 @@ void heapSort(int vetor[], int tam)
     {
 
         swap(vetor[0], vetor[i]);
-
+        cont2++;
         heap(vetor, i, 0);
+
+        cout<< "tempo de execução em segundos: "<<chrono::duration_cast<chrono::seconds>(end - start).count()<<"S"<<endl;//retorno do comando para medir o tempo de execução do codigo em segundos
     }
 }
