17c61ca915e1b1263da04a8c1ac3a5208d909dd0
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-28 18:59:05 -0300

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -1,8 +1,14 @@
+
 void SelectionSort(int vetor[], int qtde){
+    int cont=0;
+    auto start = chrono::steady_clock::now(); //comando para medir o tempo de execução do codigo
     for (int indice = 0; indice < qtde; ++indice) {
+        cont++;
         int indiceMenor = indice;
         for (int indiceSeguinte = indice+1; indiceSeguinte < qtde; ++indiceSeguinte) {
+            cont++;
             if (vetor[indiceSeguinte] < vetor[indiceMenor]) {
+                cont++;
                 indiceMenor = indiceSeguinte;
             }
         }
@@ -10,5 +16,5 @@ void SelectionSort(int vetor[], int qtde){
         vetor[indice] = vetor[indiceMenor];
         vetor[indiceMenor] = aux;
     }
+    cout<< "tempo de execução em segundos: "<<chrono::duration_cast<chrono::seconds>(end - start).count()<<"S"<<endl;//retorno do comando para medir o tempo de execução do codigo em segundos
 }
-
