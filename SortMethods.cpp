53c125158e8fd9f5880cbc79d865410754a0bb72
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-27 21:18:47 -0300

    Update SortMethods.cpp

--- SortMethods.cpp
+++ SortMethods.cpp
@@ -1,3 +1,23 @@
+#include<iostream>
+#include<random>
+#include<time.h>
+#include<stdlib.h>
+#include<locale.h>
+
+#include"Quick Sort.cpp" // o nome do cpp pra inclu
+#include"Heap Sort.cpp."
+#include"Insertion Sort.cpp."
+#include"Merge Sort.cpp"
+#include"Selection Sort.cpp"
+#include"Shell Sort.cpp"
+
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL,"");
+    srand(time(NULL));
 
     int qtde;
 
