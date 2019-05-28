61177da030313defab48769cd5de044b1afb2b81
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-28 20:44:57 -0300

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -1,21 +1,33 @@
+#include<iostream>
+#include<windows.h>
+#include<math.h>
+#include<locale.h>
+#include<chrono>
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+
+using namespace std;
 void insertionSort(int vetor[], int tamanhoVetor) {
 
 int escolhido, j, cont=0;
 
-auto start = chrono::steady_clock::now(); //comando para medir o tempo de execução do codigo
+auto start = chrono::steady_clock::now(); //comando para medir o tempo de execuÑ‡Ñƒo do codigo
 
     for (int i = tamanhoVetor; i >= 0; i--) {
-        cont++
+        cont++;
 		escolhido = vetor[i];
 		j = i + 1;
 
 		while ((j <= tamanhoVetor-1) && (vetor[j] < escolhido)) {
-            cont++
+            cont++;
 			vetor[j - 1] = vetor[j];
 			j++;
 		}
 
 		vetor[j - 1] = escolhido;
 	}
-	cout<< "tempo de execução em segundos: "<<chrono::duration_cast<chrono::seconds>(end - start).count()<<"S"<<endl;//retorno do comando para medir o tempo de execução do codigo em segundos
+    auto end = chrono::steady_clock::now();//comando para medir o tempo de execuÃ§Ã£o do codigo
+	cout<< "tempo de execuÃ§Ã£o em segundos: "<<chrono::duration_cast<chrono::seconds>(end - start).count()<<"S"<<endl;//retorno do comando para medir o tempo de execuÑ‡Ñƒo do codigo em segundos
 }
