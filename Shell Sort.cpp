68f08d0343391e2d7c04155374f9a5c7a7878f49
ThyagoGazolla <49158691+ThyagoGazolla@users.noreply.github.com>
2019-05-27 21:55:08 -0300

    Update Shell Sort.cpp

--- Shell Sort.cpp
+++ Shell Sort.cpp
@@ -5,7 +5,6 @@
 #include<string.h>
 #include<stdlib.h>
 #include<stdio.h>
-#include<time.h>
 
 using namespace std;
 
@@ -24,44 +23,4 @@ void shellSort(int vetor[], int tam){
             vetor[j] = aux;
         }
     }
-}
-
-int main()
-{
-
-    setlocale(LC_ALL,"");
-    srand(time(NULL));
-    int qtde=100;
-
-    cout << "Programa de Ordenação Insertion Sort (penultima casa)" << endl
-         << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" << endl
-         << "Gerando vetor de "<<qtde<<" números aleatórios: "<<endl;
-
-    int *vetor = new int[qtde];
-
-    for (int i = 0; i < qtde; i++){
-        vetor[i] = rand()%1001;
-    }
-
-    cout << "Pronto!" << endl;
-
-    cout << endl << "Números gerados: [";
-    for (int i = 0; i < qtde; i++)
-        cout << vetor[i] << " ";
-    cout << "]" << endl;
-
-    cout << "Ordenando... " << endl;
-
-    int passos = 0;
-    shellSort(vetor, qtde);
-
-    cout <<"Número de passos: "<< passos << " passos." << endl;
-
-    cout << "Vetor Ordenado" << endl << "[";
-    for (int i = 0; i < qtde; i++)
-        cout << vetor[i] << " ";
-    cout << "]" << endl;
-
-    delete vetor;
-    return 0;
 }
