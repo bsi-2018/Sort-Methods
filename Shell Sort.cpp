8c1cab6c55f6685601854fa23a7621b4c38d98f1
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-28 18:59:08 -0300

    Update Shell Sort.cpp

--- Shell Sort.cpp
+++ Shell Sort.cpp
@@ -1,16 +1,23 @@
 void shellSort(int vetor[], int tam){
 
-    int aux, j;
+    int aux, j, cont=0;
+
+     auto start = chrono::steady_clock::now(); //comando para medir o tempo de execução do codigo
 
     for (int gap = tam/2; gap > 0; gap /= 2){
+            cont++
         for (int i = gap; i < tam; i += 1)
         {
+            cont++;
             aux = vetor[i];
-
+            cont++;
             for (j = i; j >= gap && vetor[j - gap] > aux; j = j - gap){
                 vetor[j] = vetor[j - gap];
+                cont++;
             }
             vetor[j] = aux;
+            cont++;
         }
     }
+     cout<< "tempo de execução em segundos: "<<chrono::duration_cast<chrono::seconds>(end - start).count()<<"S"<<endl;//retorno do comando para medir o tempo de execução do codigo em segundos
 }
