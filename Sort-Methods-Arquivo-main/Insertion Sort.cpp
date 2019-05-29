ba561b3dfc10afe0147bf3a4ff9c4f202b6de8be
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-29 18:20:54 -0300

    Update Insertion Sort.cpp

--- Sort-Methods-Arquivo-main/Insertion Sort.cpp
+++ Sort-Methods-Arquivo-main/Insertion Sort.cpp
@@ -11,25 +11,30 @@
 using namespace std;
 void insertionSort(int vetor[], int tamanhoVetor) {
 
-int escolhido, j, cont=0;
+long long int cont=0, mov=0;
+int escolhido, j;
 
 auto start = chrono::steady_clock::now(); //comando para medir o tempo de execuчуo do codigo
 
     for (int i = tamanhoVetor; i >= 0; i--) {
         cont++;
 		escolhido = vetor[i];
+		mov++;
 		j = i + 1;
 
 		while ((j <= tamanhoVetor-1) && (vetor[j] < escolhido)) {
             cont++;
-
 			vetor[j - 1] = vetor[j];
+			mov++;
 			j++;
 		}
 
 		vetor[j - 1] = escolhido;
+		mov++;
 
 	}
+    cout<<"O numero de comparações é: "<<cont<<endl;
+    cout<<"O numero de movimentações é: "<<mov<<endl;
     auto end = chrono::steady_clock::now();//comando para medir o tempo de execução do codigo
-	cout<< "tempo de execução em segundos: "<<chrono::duration_cast<chrono::seconds>(end - start).count()<<"S"<<endl;//retorno do comando para medir o tempo de execuчуo do codigo em segundos
+	cout <<"tempo de execução: "<<chrono::duration_cast<chrono::seconds>(end - start).count()<<"s " <<chrono::duration_cast<chrono::milliseconds>(end - start).count()<<"ms "<<chrono::duration_cast<chrono::nanoseconds>(end - start).count()<<"ns " <<endl;
 }
