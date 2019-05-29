ab850dd5dcd71b44443307ed666b2f1de6b0bb05
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-29 18:20:49 -0300

    Update Heap Sort.cpp

--- Sort-Methods-Arquivo-main/Heap Sort.cpp
+++ Sort-Methods-Arquivo-main/Heap Sort.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+long long int cont=0, mov=0;
+
 void heap(int vetor[], int tam, int i);
 void heapSort(int vetor[], int tam);
 
@@ -19,14 +21,15 @@ void heap(int vetor[], int tam, int i)
 
     if (l < tam && vetor[l] > vetor[largest])
         largest = l;
-
+        cont++;
     if (r < tam && vetor[r] > vetor[largest])
         largest = r;
-
+        cont++;
        if (largest != i)
     {
+        cont++;
         swap(vetor[i], vetor[largest]);
-
+        mov++;
         heap(vetor, tam, largest);
     }
 }
@@ -35,13 +38,17 @@ void heapSort(int vetor[], int tam)
 {
     for (int i = tam / 2 - 1; i >= 0; i--)
         heap(vetor, tam, i);
-
+        cont++;
 
     for (int i=tam-1; i>=0; i--)
     {
-
+        cont++;
         swap(vetor[0], vetor[i]);
+        mov++;
 
         heap(vetor, i, 0);
+
+    cout<<"O numero de comparações é: "<<cont<<endl;
+    cout<<"O numero de movimentações é: "<<mov<<endl;
     }
 }
