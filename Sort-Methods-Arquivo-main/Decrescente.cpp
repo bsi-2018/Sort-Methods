c79eb6b65fefefb021d52bbb463757efedfddf87
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-29 23:38:41 -0300

    Update Decrescente.cpp

--- Sort-Methods-Arquivo-main/Decrescente.cpp
+++ Sort-Methods-Arquivo-main/Decrescente.cpp
@@ -9,29 +9,29 @@
 #include<unistd.h>
 
 using namespace std;
-int* shellSort2(int *vetor, int tam){
+
+int* shellSort3(int *vetor, int tam){
 
     int aux, j;
 
-     auto start = chrono::steady_clock::now(); //comando para medir o tempo de execuчуo do codigo
-
     for (int gap = tam/2; gap > 0; gap /= 2){
-          
+
         for (int i = gap; i < tam; i += 1)
         {
-         
             aux = vetor[i];
-         
+
             for (j = i; j >= gap && vetor[j - gap] > aux; j = j - gap){
                 vetor[j] = vetor[j - gap];
+
+
             }
             vetor[j] = aux;
-            
+
+        }
     }
 
     return vetor;
 }
-
 
 int *inverter(int *vetor, int tam)
 {
@@ -39,12 +39,14 @@ int *inverter(int *vetor, int tam)
 
     aux = new int [tam];
 
+    vetor = shellSort3(vetor,tam);
+
     for(int i=0; i<tam; i++)
     {
         aux[j-i] = vetor[i];
 
     }
 
-    delete vetor;
+
     return aux;
 }
