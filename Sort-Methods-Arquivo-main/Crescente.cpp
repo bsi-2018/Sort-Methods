7e57e3107343a128efd0dbe58b64ffc8c671c2b9
guilhermeqbs <51099303+guilhermeqbs@users.noreply.github.com>
2019-05-29 23:38:37 -0300

    Update Crescente.cpp

--- Sort-Methods-Arquivo-main/Crescente.cpp
+++ Sort-Methods-Arquivo-main/Crescente.cpp
@@ -9,24 +9,25 @@
 #include<unistd.h>
 
 using namespace std;
-int* shellSort2(int *vetor, int tam){
+
+int *shellSort2(int *vetor, int tam){
 
     int aux, j;
 
-     auto start = chrono::steady_clock::now(); //comando para medir o tempo de execuчуo do codigo
-
     for (int gap = tam/2; gap > 0; gap /= 2){
-          
+
         for (int i = gap; i < tam; i += 1)
         {
-         
             aux = vetor[i];
-         
+
             for (j = i; j >= gap && vetor[j - gap] > aux; j = j - gap){
                 vetor[j] = vetor[j - gap];
+
+
             }
             vetor[j] = aux;
-            
+
+        }
     }
 
     return vetor;
